c0b9f0e8914c716a97243ceb577cb5a3d9945ebb
alexeenkop <alexeenkop77@gmail.com>
2015-11-04 13:32:27 +0200

    same change

--- src/main/java/ua/cn/kp/service/BaseService.java
+++ src/main/java/ua/cn/kp/service/BaseService.java
@@ -2,17 +2,22 @@ package ua.cn.kp.service;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import ua.cn.kp.domain.Base;
 
+@SuppressWarnings("unchecked")
 public class BaseService<T extends Base> implements IBaseService<T>{
-
-	@Override
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
 	public void add(T t) {
 		// TODO Auto-generated method stub
-		
+		sessionFactory.getCurrentSession().save(t);
 	}
 
-	@Override
 	public void del(Integer id) {
 		// TODO Auto-generated method stub
 		
